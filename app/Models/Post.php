1f8c9c27175eb2f8853d24310c46568f92a6536f
computerguru1208 <computerguru1208@gmail.com>
2024-04-01 21:43:54 -0500

    Updated Category and Post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['title', 'slug', 'thumbmail', 'body', 'active', 'published_at', 'user_id'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(related: User::class);
+    }
+
+    public function categories(): BelongsToMany
+    {
+        return $this->belongsToMany(related: Category::class);
+    }
 }
